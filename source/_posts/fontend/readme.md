e5a70155eea290edf458a97acbb0c85b77ea5c35
chenye01 <chenye01@100tal.com>
2020-08-28 11:12:06 +0800

    webpack add

--- source/_posts/fontend/readme.md
+++ source/_posts/fontend/readme.md
@@ -4,3 +4,11 @@
 
 - js多种继承
 
+# 学习路线
+
+webpack/vue/react/qiankun
+
+electron/weex/小程序
+
+canvas/phaser/cocos
+
--- source/_posts/test/test.js
+++ source/_posts/test/test.js
@@ -1,12 +1,22 @@
 
-function parse (source, parentPath) {
-  let ast = babylon.parse(source)
-  let dependcies = []
+// npm install @babel/parse @babel/traverse @babel/core @babel/preset-env
 
-  traverse(ast, {
-    CallExpression(p) {
-      
-    }
+const fs = require('fs')
+const path = require('path')
+const traverse = require('@babel/traverse').default
+const parser = require('@babel/parser')
+const babel = require('@babel/core')
+
+
+function stepOne (filename) {
+  // read file
+  const ast = readFile(filename)
+  // 遍历ast抽象语法
+  const dependencies = traverseAST(ast)
+
+  // 通过@babel/core 和 @babel/preset-env 进行代码的转换
+  const { code } = babel.transformFromAst(ast, null, {
+    
   })
 }
 
