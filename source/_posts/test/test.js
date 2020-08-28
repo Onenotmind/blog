55307492caf24654c579da010786335c5b9171b4
chenye01 <chenye01@100tal.com>
2020-08-28 12:14:48 +0800

    webpack简单实现

--- source/_posts/test/test.js
+++ source/_posts/test/test.js
@@ -16,8 +16,36 @@ function stepOne (filename) {
 
   // 通过@babel/core 和 @babel/preset-env 进行代码的转换
   const { code } = babel.transformFromAst(ast, null, {
-    
+    presets: ['@babel/preet-env']
   })
+
+  // 返回文件名称 依赖关系
+  return {
+    filename,
+    dependencies,
+    code
+  }
 }
 
+function readFile (filename) {
+  const content = fs.readFileSync(filename, 'utf-8')
+  const ast = parser.parse(content, {
+    sourceType: 'module' // babel官方规定必须加这个参数 不然无法识别ES MODULE
+  })
+  return ast
+}
+
+function traverseAST (ast) {
+  const dependencies = {}
+  traverse(ast, {
+    // 获取通过import引入的模块
+    ImportDeclaration({code}) {
+      const dirname = path.dirname(filename)
+      const newFile = './' + path.join(dirname, node.source.value)
+      // 保存所依赖的模块
+      dependencies[node.source.value] = newFile
+    }
+  })
+  return dependencies
+}
 
