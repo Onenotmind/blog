998eed2ff3b51f3519ec92da268d8dad1f7a5333
chenye01 <chenye01@100tal.com>
2020-09-03 17:50:09 +0800

    add redux blog

--- source/_posts/fontend/awesome-react/readme.md
+++ source/_posts/fontend/awesome-react/readme.md
@@ -3,6 +3,10 @@
 
 > https://github.com/enaqx/awesome-react
 
+# Content 
+
+- [redux-and-hooks.md]() - redux and hooks
+
 ## react 组件库
 
 - [project](https://github.com/ant-design/ant-design) A UI Design Language and React UI library 
--- source/_posts/test/test.js
+++ source/_posts/test/test.js
@@ -1,51 +1,24 @@
 
-// npm install @babel/parse @babel/traverse @babel/core @babel/preset-env
-
-const fs = require('fs')
-const path = require('path')
-const traverse = require('@babel/traverse').default
-const parser = require('@babel/parser')
-const babel = require('@babel/core')
+import React, { Component, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 
-function stepOne (filename) {
-  // read file
-  const ast = readFile(filename)
-  // 遍历ast抽象语法
-  const dependencies = traverseAST(ast)
+const Main = () => {
+  const [listName, setListName] = useState('')
+  const todoList = useSelector(state => state.todoList)
 
-  // 通过@babel/core 和 @babel/preset-env 进行代码的转换
-  const { code } = babel.transformFromAst(ast, null, {
-    presets: ['@babel/preet-env']
-  })
-
-  // 返回文件名称 依赖关系
-  return {
-    filename,
-    dependencies,
-    code
+  const dispatch = useDispatch()
+  const addToDoList = () => {
+    dispatch({
+      type: 'ADD_TODOLIST',
+      payload
+    })
   }
+
+  return (
+    
+  )
 }
 
-function readFile (filename) {
-  const content = fs.readFileSync(filename, 'utf-8')
-  const ast = parser.parse(content, {
-    sourceType: 'module' // babel官方规定必须加这个参数 不然无法识别ES MODULE
-  })
-  return ast
-}
 
-function traverseAST (ast) {
-  const dependencies = {}
-  traverse(ast, {
-    // 获取通过import引入的模块
-    ImportDeclaration({code}) {
-      const dirname = path.dirname(filename)
-      const newFile = './' + path.join(dirname, node.source.value)
-      // 保存所依赖的模块
-      dependencies[node.source.value] = newFile
-    }
-  })
-  return dependencies
-}
 
